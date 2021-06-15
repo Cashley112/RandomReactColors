c9828be0d362304a7ad3568e43a231953a88b3c6
Connor Ashley <ashleycm@vcu.edu>
2021-06-15 11:33:45 -0400

    implemented Box component that is loaded with a random color, and assigned new random color through onClick property

---
+++ src/Box.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Box.css';
+import { choice } from './helpers';
+
+class Box extends Component {
+    static defaultProps = {
+        allColors: ['purple', 'magenta','violet', 'pink']
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {color: choice(this.props.allColors)};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    pickColor() {
+        let newColor = choice(this.props.allColors);
+        this.setState({ color: newColor });
+    }
+
+    handleClick() {
+        this.pickColor();
+    }
+
+    render () {
+        return (
+            <div 
+                className="Box"
+                style={{backgroundColor: this.state.color}}
+                onClick={this.handleClick}
+            >
+                
+            </div>
+        )
+    }
+}
+
+export default Box;
